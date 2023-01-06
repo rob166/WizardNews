9a321aa81fae5941db20b407d818d1dfe3b82066
Robert Rosenzweig <robgrosenzweig@gmail.com>
2023-01-06 11:44:09 -0500

    Everyting but error handling and bonus finished

--- app.js
+++ app.js
@@ -1,10 +1,97 @@
 const express = require("express");
-const app = express();
+const morgan = require("morgan");
+const postBank = require("./postBank");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const app = express();
+app.use(morgan('dev'));
+app.use(express.static('./public'))
+
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+  const html =
+  `<!DOCTYPE_html>
+  <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/>Wizard News</header>
+        ${posts.map(post => `
+          <div class="news-item">
+            <p>
+              <span class="news-position">${post.id}. ▲</span>
+                <a href="/posts/${post.id}">${post.title}</a>
+              <small>(by ${post.name})</small>
+            </p>
+            <small class="news-info">
+              ${post.upvotes} upvotes | ${post.date}
+            </small>
+          </div>`
+        ).join('')}
+      </div>
+    </body>
+  </html>`
+
+  res.send(html);
+
+});
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  if (!post.id) {
+    
+    res.status(404)
+    const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class="not-found">
+        <p>Accio Page! 🧙‍♀️ ... Page Not Found</p>
+        <img src="/dumbledore-404.gif" />
+      </div>
+    </body>
+    </html>`
+    res.send(html)
+  } else {
+
+  const html =
+  `<!DOCTYPE_html>
+  <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/>Wizard News</header>
+      <div class="news-item">
+      <p>
+        ${post.title}
+          <small>(by ${post.name})</small>
+      </p>
+      <p>
+        ${post.content}
+      </p>
+    </body>
+  </html>`
+  
+
+
+  res.send(html);
+  }
+
+});
 
 const PORT = 1337;
-
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
